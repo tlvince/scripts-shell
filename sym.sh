4693f1bf8860cb858677317107736928d79167bb
Tom Vincent <tlvince@gmail.com>
2012-04-29 21:15:58 +0100

    sym: prefix usage

--- sym.sh
+++ sym.sh
@@ -2,7 +2,7 @@
 # sym: Symlink the canonical version(s) of $@ into $HOME/bin.
 # Copyright 2010-2012 Tom Vincent <http://www.tlvince.com/>
 
-usage() { echo "${0##*/} [file] -> ~/bin/[sanitised-symlink]"; exit; }
+usage() { echo "usage: ${0##*/} [file] -> ~/bin/[sanitised-symlink]"; exit; }
 
 [[ $1 ]] || usage
 [[ $1 = "-h" ]] || [[ $1 = "--help" ]] && usage
