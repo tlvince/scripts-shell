8168684d6f27fa1e508664b05d23ebc6bf85dced
Tom Vincent <git@tlvince.com>
2013-05-26 18:38:06 +0800

    OS X and user-definable path support

--- sym.bash
+++ sym.bash
@@ -1,16 +1,23 @@
 #!/bin/bash
 # sym: Symlink the canonical version(s) of $@ into $HOME/bin.
-# Copyright 2010-2012 Tom Vincent <http://tlvince.com/contact/>
+# Copyright 2010-2013 Tom Vincent <http://tlvince.com/contact>
 
-usage() { echo "usage: ${0##*/} [file] -> ~/bin/[sanitised-symlink]"; exit; }
+usage() {
+  echo "usage: [BIN=$bin] ${0##*/} file -> $bin/{sanitised-symlink}"
+  exit
+}
 
+bin="${BIN:-$HOME/bin}"
 [[ $1 ]] || usage
 [[ $1 = "-h" ]] || [[ $1 = "--help" ]] && usage
+[[ -d "$bin" ]] || mkdir -p "$bin"
+
+# brew install coreutils
+readlink="readlink"; [[ $(uname) = "Darwin" ]] && readlink="g$readlink"
 
 for i in "$@"; do
-    full="$(readlink -f "$i")"  # Full canonical path
-    file="${full##*/}"          # Basename
-    file="${file%.*}"           # Strip extension
-    ln -s "$full" "$HOME/bin/$file"
+  full="$($readlink -f "$i")" # Full canonical path
+  file="${full##*/}"          # Basename
+  file="${file%.*}"           # Strip extension
+  ln -s "$full" "$bin/$file"
 done
-
