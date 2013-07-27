e91b94f0dd7e8f0a79bd17598f2891703a1b3482
Tom Vincent <git@tlvince.com>
2013-07-27 10:53:28 +0100

    Added bookmark.sh

---
+++ bookmark.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Simple (reference) bookmarks.
+# © 2013 Tom Vincent <http://tlvince.com/contact>
+
+BOOKMARKS="${BOOKMARKS:-$HOME/.bookmarks}"
+
+add() {
+  read -p "URL: " url
+
+  line="$(grep -n "^$url" "$BOOKMARKS" | cut -f1 -d:)"
+  if [[ "$line" =~ ^[0-9]+$ ]]; then
+    read -p "URL already bookmarked. Replace? " -n 1
+    [[ ! $REPLY =~ ^[Yy]$ ]] && exit
+    gsed -i "$line d" "$BOOKMARKS"
+  fi
+
+  read -p "Tags: " tags
+  echo "$url $tags" >> "$BOOKMARKS"
+}
+
+search() {
+  grep "$@" "$BOOKMARKS" | urlview
+}
+
+[[ $# -eq 0 ]] && add || search "$@"
