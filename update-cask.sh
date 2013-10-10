778a884988035bbf589408f0fbb30374cff3f4ed
Tom Vincent <git@tlvince.com>
2013-10-10 19:58:51 +0100

    Check cask is installed

--- update-cask.sh
+++ update-cask.sh
@@ -2,6 +2,8 @@
 # Update cask's.
 # https://github.com/phinze/homebrew-cask/issues/309#issuecomment-17972519
 
+brew cask >/dev/null 2>&1 && { [ $? -eq 0 ] || exit 1; }
+
 caskApps=$(ls /opt/homebrew-cask/Caskroom/)
 for app in $caskApps; do
   appToCheck=$(brew cask list | grep "$app")
